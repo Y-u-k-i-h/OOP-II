969f647d64b31bb93cb2f2cf16b5b14209dbd0dc
Yukih <4rnold.richard@gmail.com>
2025-04-28 14:12:50 +0300

    Done with Challenge one

---
+++ Challenges/Challenge1.java
@@ -0,0 +1,12 @@
+package Challenges;
+/* Creating a class and a method */
+
+public class Challenge1 {
+    void nice_day() {
+        System.out.println("Have a Nice day!");
+    }
+
+    public static void main (String[] args) {
+        (new Challenge1()).nice_day();
+    }
+}
